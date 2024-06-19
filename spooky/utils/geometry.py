248fa8b1aeabd34abeb615ed1778e0c7a68e5ee1
Inaki Ortiz de Landaluce <inaki.ortiz@ext.esa.int>
2024-06-19 17:02:40 +0200

    Added functions to calculate the approximate maximum distance a ground station can communicate with a satellite, and the destination geodetic coordinates after moving such distance along a given bearing/direction

--- spooky/utils/geometry.py
+++ spooky/utils/geometry.py
@@ -1,25 +1,49 @@
 import math
+from geopy.point import Point
+from geopy.distance import geodesic
 
 
-def earth_los_angle(altitude: float, elevation: float, earth_radius=6371.0084):
+def earth_los_distance(altitude: float, min_elevation: float, earth_radius=6371.0084):
     """
-    Calculates the approximate angular separation on the Earth's surface within which a satellite at a specified
-    altitude can communicate from a ground station (i.e. above a minimum elevation angle)
+    Calculates the approximate maximum distance on the Earth's surface within which a satellite at a specified
+    altitude can communicate from a ground station above a minimum elevation angle.
     Params:
         altitude: float
             Satellite's altitude in kilometers.
-        elevation: float
+        min_elevation: float
             Minimum elevation in degrees above which ground station and satellite can communicate each other.
         earth_radius: float
-            Earth radius in kilometers. Default value is the mean value of the Earth's triaxial ellipsoid.
+            Earth radius in kilometers. Default value is the mean value of the Earth's tri-axial ellipsoid.
     Returns:
-        Angular separation on the Earth's surface in degrees.
+        Maximum distance on the Earth's surface in km.
+    Example:
+        import spooky.utils.geom
+        distance = earth_los_distance(100, 15)
+        print(angle) # expected value: 334.774 km
+    """
+    angle = earth_los_angle(altitude, min_elevation, earth_radius)
+    return math.radians(angle) * earth_radius
+
+
+def earth_los_angle(altitude: float, min_elevation: float, earth_radius=6371.0084):
+    """
+    Calculates the approximate maximum angular separation on the Earth's surface within which a satellite at a specified
+    altitude can communicate from a ground station above a minimum elevation angle.
+    Params:
+        altitude: float
+            Satellite's altitude in kilometers.
+        min_elevation: float
+            Minimum elevation in degrees above which ground station and satellite can communicate each other.
+        earth_radius: float
+            Earth radius in kilometers. Default value is the mean value of the Earth's tri-axial ellipsoid.
+    Returns:
+        Maximum angular separation on the Earth's surface in degrees.
     Example:
         import spooky.utils.geom
         angle = earth_los_angle(100, 15)
         print(angle) # expected value: 3.0107 degrees
     """
-    e = math.cos(math.radians(elevation))
+    e = math.cos(math.radians(min_elevation))
     re = earth_radius
     f = re / (re + altitude)
 
@@ -29,3 +53,36 @@ def earth_los_angle(altitude: float, elevation: float, earth_radius=6371.0084):
     # theta and phi and solving the resulting quadratic equation as a function of the distance ratio, f, and the cosine
     # of the elevation, e, we get the following formula:
     return math.degrees(math.acos(e**2 * f + math.sqrt(e**4 * f**2 - e**2 * (1 + f**2) + 1)))
+
+
+def move_along_earth_surface(lon: float, lat: float, altitude: float, distance: float, bearing: float):
+    """
+    Move along Earth's surface a given distance from a starting position along a given direction via the corresponding
+    geodesic.
+    Params:
+        lon: float
+            Longitude of the starting position in degrees.
+        lat: float
+            Latitude of the starting position in degrees.
+        altitude: float
+            Altitude of the starting position in kilometers.
+        distance: float
+            The distance of the displacement in kilometers.
+        bearing: float
+            Bearing in degrees: 0 - North, 90 - East, 180 - South, 270 or -90 West.
+    Returns:
+        point (float, float, float)
+        The longitude, latitude and altitude coordinates of the destination point using a starting position, bearing
+        and a distance.
+    Example:
+         from spooky.utils.geometry import *
+         distance = earth_los_distance(100, 15)
+         lon, lat, alt = move_along_earth_surface(-3.319995, 55.909723, 0, distance, 0)
+         print(lon. lat, alt)  # expected values: -3.319995, 58.915760, 0
+    """
+    # FIXME replace by analytic formula to get rid of geopy dependency
+    p = Point(lat, lon, altitude)
+    dest = geodesic(kilometers=distance).destination(p, bearing=bearing)  # uses WGS-84, aligned with spice
+    return dest.longitude, dest.latitude, dest.altitude
+
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from spooky.utils.geometry import *
 
 
@@ -15,7 +16,19 @@ class TestGeometry(unittest.TestCase):
     def test_earth_los_angle(self):
         angle = earth_los_angle(100, 15)
         expected_angle = 3.0107
-        self.assertAlmostEqual(angle, expected_angle, places=4)
+        self.assertAlmostEqual(angle, expected_angle, places=3)
+
+    def test_earth_los_distance(self):
+        distance = earth_los_distance(100, 15)
+        expected_distance = 334.774
+        self.assertAlmostEqual(distance, expected_distance, places=2)
+
+    def test_movel_along_earth(self):
+        distance = 334.774
+        lon, lat, alt = move_along_earth_surface(-3.319995, 55.909723, 0, distance, 0)
+        coordinates = np.array((lon, lat, alt))
+        expected_coordinates = np.array((-3.319995, 58.915760, 0))
+        np.testing.assert_almost_equal(coordinates, expected_coordinates, decimal=3)
 
 
 if __name__ == '__main__':
